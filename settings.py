851ea6bd84d39ae87fdbaec21a6fa8bc8495a595
Bayyavarapu Naveen <naveennaveen92435@gmail.com>
2026-03-31 02:39:18 +0530

    fix render 400 allowed hosts

--- settings.py
+++ settings.py
@@ -7,8 +7,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-change-t
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,.onrender.com,movie-booking-app-nlr1.onrender.com'
+).split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://movie-booking-app-nlr1.onrender.com'
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
