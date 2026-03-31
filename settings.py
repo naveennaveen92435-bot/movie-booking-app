e9ddfc4eb8528d90f32af82e8ce58003effcceca
Bayyavarapu Naveen <naveennaveen92435@gmail.com>
2026-04-01 01:34:00 +0530

    fix: whitenoise autorefresh for admin CSS

--- settings.py
+++ settings.py
@@ -106,7 +106,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+WHITENOISE_AUTOREFRESH = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
